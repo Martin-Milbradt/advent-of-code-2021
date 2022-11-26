1043dce0353c67e99021ddc3d96e1d5ad3cbbf5e
Milli <onethenumbernottheword@gmail.com>
2022-11-26 20:45:40 +0100

    1.1: fix

--- 1 - sonar sweep.py
+++ 1 - sonar sweep.py
@@ -1,14 +1,12 @@
 import modules
 
 modules.init()
-data = modules.get_data_string("1")
+data = modules.get_data("1", int)
 last = False
 increases = 0
 for val in data:
     if last:
         increases += val > last
-        if val <= last:
-            print(f"val - last = {val - last}")
     last = val
 
 print(increases)
