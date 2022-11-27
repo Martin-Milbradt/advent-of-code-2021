ff55828017630e33f2dccd102e0e0df6b1e77083
Milli <onethenumbernottheword@gmail.com>
2022-11-27 17:12:37 +0100

    specify return type

--- 0 - tests.py
+++ 0 - tests.py
@@ -3,7 +3,7 @@ import modules
 modules.init()
 
 
-def test_get_int():
+def test_get_int() -> None:
     # data = modules.get_data_string("1") # error
     data = modules.get_data("1", int)
     print(sum(data[0:5]))
